c29b2befff44b5c1345d8606f5f71dcb50b05cf0
Elias Gajo <frisoulion@hotmail.com>
2025-04-29 08:34:04 +0200

    export email publipostage

--- enregistrement-audio/src/app/export-coproprietaire/export-coproprietaire.component.ts
+++ enregistrement-audio/src/app/export-coproprietaire/export-coproprietaire.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DataframeComponent } from '../dataframe/dataframe.component';
 import { FormsModule } from '@angular/forms';
+import * as ExcelJS from 'exceljs';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-export-coproprietaire',
@@ -16,25 +18,62 @@ export class ExportCoproprietaireComponent {
   copied = false;
 
   compute_liste_email() {
-    this.liste_email = [];
-    
+    const emails = this.data_filtered
+      .map(item => item['NOEMAI'])
+      .filter(email => !!email?.trim())
+      .map(email => email.trim().toLowerCase());
+
+    // Supprimer les doublons
+    this.liste_email = Array.from(new Set(emails));
   }
 
   compute_email_str() {
     this.email_str = "";
-    for (let data of this.data_filtered) {
-      let email = data['NOEMAI'];
-      if(email.length > 0){
-        if (this.email_str.length > 0) {
-          this.email_str += ";";
-        }
-        this.email_str += email;
+    for (let email of this.liste_email) {
+      if (this.email_str.length > 0) {
+        this.email_str += ";";
       }
+      this.email_str += email;
     }
+  }
+  
+  clean_data_for_excel(data_to_clean: any) {
+    let cleaned_data: any[] = [];
+    for(let i = 0; i < data_to_clean.length; i ++) {
+      let item_to_clean = data_to_clean[i];
+      let cleaned_item: any = {}
+      for(let key in item_to_clean) {
+        const value = item_to_clean[key];
+        if(value === null || value === undefined) {
+          cleaned_item[key] = '';
+        } else {
+          cleaned_item[key] = value;
+        }
+      }
+      cleaned_data.push(cleaned_item);
+    }
+    return cleaned_data;
   }
 
   exporter_liste_email() {
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('Emails');
+    const headers = ['Emails'];
+    worksheet.addRow(headers);
 
+    this.liste_email.forEach(item => {
+      const row = [];
+      row.push(item);
+      worksheet.addRow(row);
+    });
+
+    workbook.xlsx.writeBuffer().then((buffer: any) => {
+      const blob = new Blob([buffer], {
+        type:
+          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      });
+      FileSaver.saveAs(blob, `Emails.xlsx`);
+    });
   }
 
   update_data_filtered(data_filtered: any[]) {
