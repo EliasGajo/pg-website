783bf80793a130e6396e9706e4d7f4526a7d83c7
Elias Gajo <frisoulion@hotmail.com>
2026-01-21 15:04:31 +0100

    extraction email

--- enregistrement-audio/src/app/export-email/export-email.component.ts
+++ enregistrement-audio/src/app/export-email/export-email.component.ts
@@ -1,5 +1,6 @@
 import { Component, NgZone } from '@angular/core';
 import { DataframeComponent } from '../dataframe/dataframe.component';
+import { ExportExcelService } from '../services/export-excel.service';
 
 @Component({
   selector: 'app-export-email',
@@ -12,7 +13,7 @@ export class ExportEmailComponent {
   data_filtered: any[] = [];
   traductions: {[key:string]:string} = {};
 
-  constructor(private zone: NgZone) {}
+  constructor(private zone: NgZone, private exportExcelService: ExportExcelService) {}
 
   ngOnInit() {
     this.zone.run(() => {
@@ -37,6 +38,38 @@ export class ExportEmailComponent {
 
   update_traductions(new_traductions: {[key:string]:string}) {
     this.traductions = new_traductions;
+  }
+
+  export_concept_b180() {
+    var email_nom_prenom = this.data_filtered.map(data => {
+      var email: string = data['De: (adresse)'] || '';
+
+      const nom_email: string = data['De: (nom)'] || '';
+      var nom = '';
+      var prenom = '';
+
+      if(email == "wordpress@concept-b180.ch"){
+        const body: string =
+          data['Corps'] || '';
+
+        const nomMatch = body.match(/Nom\s*:\s*([^\n\r]+)/i);
+        const prenomMatch = body.match(/Pr[eé]nom\s*:\s*([^\n\r]+)/i);
+        const emailMatch = body.match(
+          /From\s*:\s*.*?([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/i
+        );
+        nom = nomMatch ? nomMatch[1].trim() : nom;
+        prenom = prenomMatch ? prenomMatch[1].trim() : prenom;
+        email = emailMatch ? emailMatch[1].trim() : email;
+      }
+
+      return {
+        email,
+        nom_email,
+        nom_extrait: nom,
+        prenom_extrait: prenom
+      };
+    });
+    this.exportExcelService.exporter_table(email_nom_prenom, 'Email nom prenom', this.traductions);
   }
 }
 
